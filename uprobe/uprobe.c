014e72b91ff6f9fe3f1545ad1e683b8f7acbcc85
WYuei <392965713@qq.com>
2022-06-23 15:50:23 +0800

    init

---
+++ uprobe/uprobe.c
@@ -0,0 +1,28 @@
+// +build ignore
+
+#include "common.h"
+
+#include "bpf_tracing.h"
+
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+struct event {
+	u32 pid;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+} events SEC(".maps");
+
+const struct event *unused __attribute__((unused));
+
+SEC("uprobe/http_main")
+int uprobe_http_main(struct pt_regs *ctx) {
+	struct event event;
+
+	event.pid = bpf_get_current_pid_tgid();
+
+	bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
+
+	return 0;
+}

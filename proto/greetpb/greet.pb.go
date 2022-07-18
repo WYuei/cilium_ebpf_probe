5c271d863cca99c4aa79699e098a8c909d091b38
WYuei <37688911+WYuei@users.noreply.github.com>
2022-07-18 15:48:45 +0800

    Update greet.pb.go

--- proto/greetpb/greet.pb.go
+++ proto/greetpb/greet.pb.go
@@ -21,7 +21,7 @@
 // 	protoc        v3.6.1
 // source: proto/greet.proto
 
-package greetpb
+package github.com/WYuei/cilium_ebpf_probe/proto/greetpb
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
